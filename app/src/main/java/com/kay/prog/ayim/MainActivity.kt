f101fa741ab1c052bc06e8f51b4b20d2d98241e6
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2021-11-24 14:30:32 +0600

    hw/L8 done

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -2,10 +2,35 @@ package com.kay.prog.ayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layout = findViewById<ConstraintLayout>(R.id.main_layout)
+        val textView = findViewById<TextView>(R.id.txt)
+        val redButton = findViewById<AppCompatButton>(R.id.red_button)
+        val yellowButton = findViewById<AppCompatButton>(R.id.yellow_button)
+        val greenButton = findViewById<AppCompatButton>(R.id.green_button)
+
+        redButton.setOnClickListener {
+            layout.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+            textView.text = "Stop!"
+        }
+
+        yellowButton.setOnClickListener {
+            layout.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow))
+            textView.text = "Wait"
+        }
+
+        greenButton.setOnClickListener {
+            layout.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+            textView.text = "Go!"
+        }
     }
 }
