b1561887943fb9ae0ca8eae28490a581b7151927
Ulanbekova_Kamila <ulanbekova_ka@auca.kg>
2021-12-12 15:38:20 +0600

    binding to calc

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -3,88 +3,65 @@ package com.kay.prog.ayim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
+import com.kay.prog.ayim.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.Exception
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     private var expression = StringBuilder("")
     private var lastInp = ' '
     private var isBracketOpened = false
-    private lateinit var inpTxt: AppCompatTextView
-    private lateinit var resultTxt: AppCompatTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        inpTxt = findViewById(R.id.input_txt)
+        binding.apply {
+            oneButton.setOnClickListener(::onClick)
+            twoButton.setOnClickListener(::onClick)
+            threeButton.setOnClickListener(::onClick)
+            fourButton.setOnClickListener(::onClick)
+            fiveButton.setOnClickListener(::onClick)
+            sixButton.setOnClickListener(::onClick)
+            sevenButton.setOnClickListener(::onClick)
+            eightButton.setOnClickListener(::onClick)
+            nineButton.setOnClickListener(::onClick)
+            zeroButton.setOnClickListener(::onClick)
+            addButton.setOnClickListener(::onClick)
+            subtractButton.setOnClickListener(::onClick)
+            multiplyButton.setOnClickListener(::onClick)
+            divideButton.setOnClickListener(::onClick)
+            dotButton.setOnClickListener(::onClick)
+            allClearButton.setOnClickListener(::onClick)
+            openBracketButton.setOnClickListener(::onClick)
+            closeBracketButton.setOnClickListener(::onClick)
+            goBackButton.setOnClickListener(::onClick)
 
-        val oneButton = findViewById<AppCompatButton>(R.id.one_button)
-        oneButton.setOnClickListener(::onClick)
-        val twoButton = findViewById<AppCompatButton>(R.id.two_button)
-        twoButton.setOnClickListener(::onClick)
-        val threeButton = findViewById<AppCompatButton>(R.id.three_button)
-        threeButton.setOnClickListener(::onClick)
-        val fourButton = findViewById<AppCompatButton>(R.id.four_button)
-        fourButton.setOnClickListener(::onClick)
-        val fiveButton = findViewById<AppCompatButton>(R.id.five_button)
-        fiveButton.setOnClickListener(::onClick)
-        val sixButton = findViewById<AppCompatButton>(R.id.six_button)
-        sixButton.setOnClickListener(::onClick)
-        val sevenButton = findViewById<AppCompatButton>(R.id.seven_button)
-        sevenButton.setOnClickListener(::onClick)
-        val eightButton = findViewById<AppCompatButton>(R.id.eight_button)
-        eightButton.setOnClickListener(::onClick)
-        val nineButton = findViewById<AppCompatButton>(R.id.nine_button)
-        nineButton.setOnClickListener(::onClick)
-        val zeroButton = findViewById<AppCompatButton>(R.id.zero_button)
-        zeroButton.setOnClickListener(::onClick)
-        val addButton = findViewById<AppCompatButton>(R.id.add_button)
-        addButton.setOnClickListener(::onClick)
-        val subtractButton = findViewById<AppCompatButton>(R.id.subtract_button)
-        subtractButton.setOnClickListener(::onClick)
-        val multiplyButton = findViewById<AppCompatButton>(R.id.multiply_button)
-        multiplyButton.setOnClickListener(::onClick)
-        val divideButton = findViewById<AppCompatButton>(R.id.divide_button)
-        divideButton.setOnClickListener(::onClick)
-        val dotButton = findViewById<AppCompatButton>(R.id.dot_button)
-        dotButton.setOnClickListener(::onClick)
-        val clearButton = findViewById<AppCompatButton>(R.id.all_clear_button)
-        clearButton.setOnClickListener(::onClick)
-        val openButton = findViewById<AppCompatButton>(R.id.open_bracket_button)
-        openButton.setOnClickListener(::onClick)
-        val closeButton = findViewById<AppCompatButton>(R.id.close_bracket_button)
-        closeButton.setOnClickListener(::onClick)
-        val backButton = findViewById<AppCompatButton>(R.id.go_back_button)
-        backButton.setOnClickListener(::onClick)
-
-        resultTxt = findViewById(R.id.solution_txt)
-        val equalButton = findViewById<AppCompatButton>(R.id.equal_button)
-        equalButton.setOnClickListener {
-
-            if (!lastInp.isDigit() || isBracketOpened) {
-                resultTxt.text = "Error: Incorrect syntax"
-            } else {
-                try {
-                    val result = ExpressionBuilder(expression.toString()).build().evaluate()
-                    val resToInt = result.toInt()
-                    if ((result * 10).toInt() == resToInt * 10) {
-                        resultTxt.text = resToInt.toString()
-                    } else {
-                        resultTxt.text = result.toString()
+            equalButton.setOnClickListener {
+                if (!lastInp.isDigit() || isBracketOpened) {
+                    solutionTxt.text = "Error: Incorrect syntax"
+                } else {
+                    try {
+                        val result = ExpressionBuilder(expression.toString()).build().evaluate()
+                        val resToInt = result.toInt()
+                        if ((result * 10).toInt() == resToInt * 10) {
+                            solutionTxt.text = resToInt.toString()
+                        } else {
+                            solutionTxt.text = result.toString()
+                        }
+                    } catch (e: Exception) {
+                        solutionTxt.text = e.message
                     }
-                } catch (e: Exception) {
-                    resultTxt.text = e.message
                 }
             }
         }
     }
 
     private fun onClick(view: View) {
-
         when(view.id) {
             R.id.one_button -> {
                 expression.append('1')
@@ -155,7 +132,7 @@ class MainActivity : AppCompatActivity() {
             R.id.all_clear_button -> {
                 if (expression.isNotEmpty()) {
                     expression.clear()
-                    resultTxt.text = ""
+                    binding.solutionTxt.text = ""
                     lastInp = ' '
                 }
             }
@@ -182,11 +159,16 @@ class MainActivity : AppCompatActivity() {
             R.id.go_back_button -> {
                 if (expression.isNotEmpty()) {
                     expression.deleteCharAt(expression.length - 1)
-                    lastInp = expression.elementAt(expression.length - 1)
+
+                    lastInp = if (expression.isNotEmpty()) {
+                        expression.elementAt(expression.length - 1)
+                    } else {
+                        ' '
+                    }
                 }
             }
         }
 
-        inpTxt.text = expression.toString()
+        binding.inputTxt.text = expression.toString()
     }
 }
