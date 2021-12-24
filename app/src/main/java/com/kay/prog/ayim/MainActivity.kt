3ca4eadb975269011bd6011e345baa221d813a06
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2021-12-24 18:13:25 +0600

    find num of words

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -2,10 +2,27 @@ package com.kay.prog.ayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txt = findViewById<AppCompatTextView>(R.id.txt)
+        val edit = findViewById<AppCompatEditText>(R.id.edit)
+        val btn = findViewById<AppCompatButton>(R.id.btn)
+
+        btn.setOnClickListener {
+            val text = edit.text.toString().trim()
+            val numOfWords = if(text == "") {
+                0
+            } else {
+                text.split("\\s+".toRegex()).size
+            }
+            txt.text = "$numOfWords"
+        }
     }
 }
