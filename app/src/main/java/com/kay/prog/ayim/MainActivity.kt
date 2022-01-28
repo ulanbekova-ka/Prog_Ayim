6c872836ffbca9ac0df2be3a0b3d2037d5a82659
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2022-01-28 17:27:28 +0600

    count words

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -12,13 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val txt = findViewById<AppCompatTextView>(R.id.txt)
+        val txtKotlin = findViewById<AppCompatTextView>(R.id.txt_kotlin)
         val edit = findViewById<AppCompatEditText>(R.id.edit)
         val btn = findViewById<AppCompatButton>(R.id.btn)
 
         btn.setOnClickListener {
             val text = edit.text.toString().trim()
-            val numOfWords = text.split("\\b[а-яА-Я]*[а][я]\\b".toRegex()).size - 1
-            txt.text = "$numOfWords"
+            val numOfWords = text.split("\\b([A-Za-z][A-Za-z][A-Za-z])\\b".toRegex()).size - 1
+            txt.text = "Number of words\n consisting of\n 3 characters: $numOfWords"
+
+            val numOfKotlin = text.split("Kotlin".toRegex()).size - 1
+            txtKotlin.text = "Number of 'Kotlin's: $numOfKotlin"
         }
     }
 }
