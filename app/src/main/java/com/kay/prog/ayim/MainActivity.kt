d123edc37fa0759b672ecfdfc49601a7b1c023ba
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2022-01-28 18:00:03 +0600

    to *

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -2,37 +2,28 @@ package com.kay.prog.ayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
-import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val txt = findViewById<AppCompatTextView>(R.id.txt)
         val edit = findViewById<AppCompatEditText>(R.id.edit)
         val btn = findViewById<AppCompatButton>(R.id.btn)
 
         btn.setOnClickListener {
-            val text = edit.text.toString().trim()
-            val listOfWords = text.split("\\s+".toRegex())
-            var maxAs = 0
-            var word = ""
+            var text = edit.text.toString()
+            var temp = text.replace(',', '*')
+            text = temp.replace(':', '*')
+            temp = text.replace(';', '*')
+            text = temp.replace('!', '*')
+            temp = text.replace('?', '*')
+            text = temp.replace('.', '*')
 
-            listOfWords.forEach {
-                if (it.contains('a')) {
-                    val numOfAs = it.count{c -> c == 'a'}
-                    if (maxAs < numOfAs)
-                    {
-                        maxAs = numOfAs
-                        word = it
-                    }
-                }
-            }
-
-            txt.text = "number of maximum 'a's = $maxAs\n in the word: $word"
+            edit.text = Editable.Factory.getInstance().newEditable(text)
         }
     }
 }
