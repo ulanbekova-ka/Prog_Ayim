be595f27c008e0670fa761840e4bef7120cc4180
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2022-02-25 17:51:50 +0600

    template for retrofit

---
+++ app/src/main/java/com/kay/prog/ayim/App.kt
@@ -0,0 +1,67 @@
+package com.kay.prog.ayim
+
+import android.app.Application
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class App : Application() {
+
+    private val isDebug get() = BuildConfig.DEBUG
+
+    lateinit var githubApi: Api
+
+    override fun onCreate() {
+        super.onCreate()
+        mInstance = this
+
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor())
+            .addInterceptor(httpHeaderLoggingInterceptor())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_MOCK)
+            .client(okHttpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        githubApi = retrofit.create(Api::class.java)
+    }
+
+    private fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor(logger = object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                Log.d("OkHttp_body", message)
+            }
+        }).setLevel(if (isDebug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE)
+    }
+
+    private fun httpHeaderLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor(logger = object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                Log.d("OkHttp_header", message)
+            }
+        }).setLevel(if (isDebug) HttpLoggingInterceptor.Level.HEADERS else HttpLoggingInterceptor.Level.NONE)
+    }
+
+    companion object {
+        const val BASE_MOCK = "https://5a8507bd-1cec-40bc-b9f5-605b5b73e40a.mock.pstmn.io"
+        const val BASE_URL = "https://api.github.com/"
+        const val TIMEOUT = 300L
+
+        private var mInstance: App? = null
+        val instance get() = mInstance!!
+    }
+}
+
+val Any.Injector: App
+    get() = App.instance
--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -3,13 +3,29 @@ package com.kay.prog.ayim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kay.prog.ayim.databinding.ActivityMainBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val githubApi get() = Injector.githubApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        githubApi.errorSample()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSuccess {
+//                Log.e("TAG", "Result= $it")
+            }
+            .doOnError {
+//                Log.e("TAG", "Error= $it")
+            }
+            .subscribe()
+
     }
 }
