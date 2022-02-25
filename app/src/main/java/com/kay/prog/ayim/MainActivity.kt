c470f545b077d9af1a8f331011a5b93fa72582a9
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2022-02-25 18:45:02 +0600

    traffic lights

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -3,13 +3,43 @@ package com.kay.prog.ayim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kay.prog.ayim.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    enum class States { DEFAULT, GREEN, YELLOW, RED }
+    private lateinit var state : States
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        state = States.DEFAULT
+        binding.apply {
+            while (true) {
+                Timer().schedule(5000) {
+                    when (state) {
+                        States.DEFAULT, States.GREEN -> {
+                            state = States.RED
+                            img.setImageResource(R.drawable.ic_traffic_red)
+                            txt.text = "STOP"
+                        }
+                        States.RED -> {
+                            state = States.YELLOW
+                            img.setImageResource(R.drawable.ic_traffic_yellow)
+                            txt.text = "WAIT"
+                        }
+                        States.YELLOW -> {
+                            state = States.GREEN
+                            img.setImageResource(R.drawable.ic_traffic_green)
+                            txt.text = "GO"
+                        }
+                    }
+                }
+            }
+        }
     }
 }
