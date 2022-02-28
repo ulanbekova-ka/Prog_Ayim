4604f43eaaee3b47fc34666b9aa8c3ec9e3a5a70
Kamila Ulanbekova <ulanbekova_ka@auca.kg>
2022-02-28 17:55:29 +0600

    traffic lights

--- app/src/main/java/com/kay/prog/ayim/MainActivity.kt
+++ app/src/main/java/com/kay/prog/ayim/MainActivity.kt
@@ -2,14 +2,14 @@ package com.kay.prog.ayim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.kay.prog.ayim.databinding.ActivityMainBinding
-import java.util.*
-import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    enum class States { DEFAULT, GREEN, YELLOW, RED }
+    enum class States { GREEN, YELLOW, RED }
     private lateinit var state : States
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,29 +17,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        state = States.DEFAULT
-        binding.apply {
-            while (true) {
-                Timer().schedule(5000) {
-                    when (state) {
-                        States.DEFAULT, States.GREEN -> {
-                            state = States.RED
-                            img.setImageResource(R.drawable.ic_traffic_red)
-                            txt.text = "STOP"
-                        }
-                        States.RED -> {
-                            state = States.YELLOW
-                            img.setImageResource(R.drawable.ic_traffic_yellow)
-                            txt.text = "WAIT"
-                        }
-                        States.YELLOW -> {
-                            state = States.GREEN
-                            img.setImageResource(R.drawable.ic_traffic_green)
-                            txt.text = "GO"
-                        }
+        state = States.RED
+        object : CountDownTimer(1000000000, 5000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                when (state) {
+                    States.GREEN -> {
+                        state = States.RED
+                        binding.img.setImageResource(R.drawable.ic_traffic_red)
+                        binding.txt.text = "STOP"
+                    }
+                    States.RED -> {
+                        state = States.YELLOW
+                        binding.img.setImageResource(R.drawable.ic_traffic_yellow)
+                        binding.txt.text = "WAIT"
+                    }
+                    States.YELLOW -> {
+                        state = States.GREEN
+                        binding.img.setImageResource(R.drawable.ic_traffic_green)
+                        binding.txt.text = "GO"
                     }
                 }
             }
-        }
+
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity, "time is off", Toast.LENGTH_LONG).show()
+            }
+        }.start()
     }
 }
